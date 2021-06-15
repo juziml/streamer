26d530c42a5fd3cd81600902a39362a75c4eb0bc
sink5 <harkben50@gmail.com>
2021-06-16 00:05:17 +0800

    无限动画

--- app/src/main/java/com/z/streamer/StreamerView.kt
+++ app/src/main/java/com/z/streamer/StreamerView.kt
@@ -1,5 +1,7 @@
 package com.z.streamer
 
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.content.Context
 import android.content.res.Resources
 import android.graphics.*
@@ -19,7 +21,7 @@ class StreamerView(context: Context, attributeSet: AttributeSet) : View(context,
 
     private val paintTxt = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         textSize = 20F.dp
-        color = Color.BLACK
+        color = Color.parseColor("#dbdbdb")
     }
 
     private val paintStreamer = Paint(Paint.ANTI_ALIAS_FLAG).apply {
@@ -32,6 +34,8 @@ class StreamerView(context: Context, attributeSet: AttributeSet) : View(context,
     private val contentBitmap by lazy {
         Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
     }
+    private val streamerWidth = 30F.dp
+    private val streamerHeightOffset = 10F.dp
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
@@ -41,23 +45,18 @@ class StreamerView(context: Context, attributeSet: AttributeSet) : View(context,
         }
         val txt = "十年可期"
         val img = BitmapFactory.decodeResource(
-            context.resources, R.drawable.ic_bar_chart, BitmapFactory.Options()
-        )
+            context.resources, R.drawable.ic_cup, BitmapFactory.Options()
+        )//不可以加载Vector资源
         val txtWidth = paintTxt.measureText(txt)
-        canvas.drawText(txt, 0F, height / 2F, paintTxt)
+        val startX =  (width/2) - (txtWidth + img.width)/2F
         val imgTop = height / 2F - (img.height / 2F)
-        canvas.drawBitmap(img, txtWidth, imgTop, paintTxt)
+        canvas.drawText(txt, startX, height / 2F, paintTxt)
+        canvas.drawBitmap(img, startX+txtWidth, imgTop, paintTxt)
 
     }
 
 
-    val path = Path().also {
-        it.moveTo(10F.dp, 0F)
-        it.lineTo(40F.dp, 0F)
-        it.lineTo(40F.dp, 100F.dp)
-        it.lineTo(10F.dp, 100F.dp)
-        it.close()
-    }
+    val path = Path()
 
     val xfermode = PorterDuffXfermode(PorterDuff.Mode.DST_ATOP)
 
@@ -67,16 +66,47 @@ class StreamerView(context: Context, attributeSet: AttributeSet) : View(context,
     //不能加载vector drawable
 
     override fun onDraw(canvas: Canvas) {
+        canvas.drawColor(Color.LTGRAY)
         //开启离屏缓冲
         val count = canvas.saveLayer(0F,0F,width.toFloat(),height.toFloat(),null)
+        refreshPath(progress)
         canvas.drawPath(path,paintStreamer)
         mergePaint.xfermode = xfermode
         canvas.drawBitmap(contentBitmap,0F,0F,mergePaint)//内容
         mergePaint.xfermode = null
         canvas.restoreToCount(count)
+
+    }
+    val floatAnim = ObjectAnimator.ofFloat(0F,1F).apply {
+        duration = 1000
+        repeatMode = ValueAnimator.RESTART
+        repeatCount = ValueAnimator.INFINITE
     }
 
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        floatAnim.addUpdateListener {
+            progress = it.animatedValue as Float
+            postInvalidate()
+        }
+        floatAnim.start()
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        floatAnim.cancel()
+    }
+
+    var progress = 0F
     private fun refreshPath(progress: Float) {
+        path.reset()
+        val startX = progress * width
+        path.moveTo(startX,0F)
+        path.lineTo(startX + streamerWidth,streamerHeightOffset)
+        path.lineTo(startX+streamerWidth,height.toFloat())
+        path.lineTo(startX,height - streamerHeightOffset)
+        path.close()
+
 
     }
 }
