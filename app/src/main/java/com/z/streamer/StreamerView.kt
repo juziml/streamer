e38f00d444a035772c38091bf57fab9a12babe22
sink5 <harkben50@gmail.com>
2021-06-16 00:43:03 +0800

    提供图片缩小，支持文字偏移

--- app/src/main/java/com/z/streamer/StreamerView.kt
+++ app/src/main/java/com/z/streamer/StreamerView.kt
@@ -9,6 +9,9 @@ import android.graphics.drawable.BitmapDrawable
 import android.util.AttributeSet
 import android.util.Log
 import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.LinearInterpolator
 
 private val Int.dp get() = (this * (0.5F + Resources.getSystem().displayMetrics.density)).toInt()
 private val Float.dp get() = this * (0.5F + Resources.getSystem().displayMetrics.density)
@@ -18,24 +21,34 @@ fun String.log() {
 }
 
 class StreamerView(context: Context, attributeSet: AttributeSet) : View(context, attributeSet) {
+    val path = Path()
+    val xfermode = PorterDuffXfermode(PorterDuff.Mode.DST_ATOP)
+    private val streamerWidth = 30F.dp
+    private val streamerHeightOffset = 10F.dp
+
+    private val imgWidth = 30.dp
+    private val txtHeightOffset = 4F.dp
 
     private val paintTxt = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-        textSize = 20F.dp
+        textSize = 16F.dp
+        typeface = Typeface.DEFAULT_BOLD
         color = Color.parseColor("#dbdbdb")
     }
 
     private val paintStreamer = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         strokeCap = Paint.Cap.ROUND
         style = Paint.Style.FILL
-        color = Color.RED
+        color = Color.parseColor("#e6e6e6")
     }
     private val mergePaint = Paint(Paint.ANTI_ALIAS_FLAG)
 
     private val contentBitmap by lazy {
         Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
     }
-    private val streamerWidth = 30F.dp
-    private val streamerHeightOffset = 10F.dp
+
+    init {
+        "img.width = $imgWidth".log()
+    }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
@@ -43,22 +56,30 @@ class StreamerView(context: Context, attributeSet: AttributeSet) : View(context,
         val canvas = Canvas(contentBitmap).apply {
             density = 0
         }
-        val txt = "十年可期"
-        val img = BitmapFactory.decodeResource(
-            context.resources, R.drawable.ic_cup, BitmapFactory.Options()
+        val txt = "歇会儿 "
+        val options = BitmapFactory.Options()
+        options.inJustDecodeBounds = true
+        BitmapFactory.decodeResource(
+            context.resources, R.drawable.ic_cup, options
         )//不可以加载Vector资源
+        options.inJustDecodeBounds = false
+        options.inDensity = options.outWidth
+        options.inTargetDensity = imgWidth
+        val img = BitmapFactory.decodeResource(
+            context.resources, R.drawable.ic_cup, options
+        )
+        "img.width = ${img.width} img.height=${img.height}".log()
         val txtWidth = paintTxt.measureText(txt)
-        val startX =  (width/2) - (txtWidth + img.width)/2F
+        val textBounds = Rect()
+        paintTxt.getTextBounds(txt, 0, txt.length, textBounds)
+        val txtY = (height / 2F) - (textBounds.top + textBounds.bottom) / 2 + txtHeightOffset
+        val startX = (width / 2) - (txtWidth + img.width) / 2F
         val imgTop = height / 2F - (img.height / 2F)
-        canvas.drawText(txt, startX, height / 2F, paintTxt)
-        canvas.drawBitmap(img, startX+txtWidth, imgTop, paintTxt)
+        canvas.drawText(txt, startX, txtY, paintTxt)
+        canvas.drawBitmap(img, startX + txtWidth, imgTop, paintTxt)
 
     }
 
-
-    val path = Path()
-
-    val xfermode = PorterDuffXfermode(PorterDuff.Mode.DST_ATOP)
 
     //发现每次用的时候都搞不清楚谁往谁身上盖
     //一般都是双图合成，为了能选对MODE,先画表层Destination img 后画底层source img (反正来就对了)
@@ -66,21 +87,22 @@ class StreamerView(context: Context, attributeSet: AttributeSet) : View(context,
     //不能加载vector drawable
 
     override fun onDraw(canvas: Canvas) {
-        canvas.drawColor(Color.LTGRAY)
         //开启离屏缓冲
-        val count = canvas.saveLayer(0F,0F,width.toFloat(),height.toFloat(),null)
+        val count = canvas.saveLayer(0F, 0F, width.toFloat(), height.toFloat(), null)
         refreshPath(progress)
-        canvas.drawPath(path,paintStreamer)
+        canvas.drawPath(path, paintStreamer)
         mergePaint.xfermode = xfermode
-        canvas.drawBitmap(contentBitmap,0F,0F,mergePaint)//内容
+        canvas.drawBitmap(contentBitmap, 0F, 0F, mergePaint)//内容
         mergePaint.xfermode = null
         canvas.restoreToCount(count)
 
     }
-    val floatAnim = ObjectAnimator.ofFloat(0F,1F).apply {
-        duration = 1000
+
+    val floatAnim = ObjectAnimator.ofFloat(0F, 1F).apply {
+        duration = 1400
         repeatMode = ValueAnimator.RESTART
         repeatCount = ValueAnimator.INFINITE
+        interpolator = LinearInterpolator()
     }
 
     override fun onFinishInflate() {
@@ -101,12 +123,10 @@ class StreamerView(context: Context, attributeSet: AttributeSet) : View(context,
     private fun refreshPath(progress: Float) {
         path.reset()
         val startX = progress * width
-        path.moveTo(startX,0F)
-        path.lineTo(startX + streamerWidth,streamerHeightOffset)
-        path.lineTo(startX+streamerWidth,height.toFloat())
-        path.lineTo(startX,height - streamerHeightOffset)
+        path.moveTo(startX, 0F)
+        path.lineTo(startX + streamerWidth, streamerHeightOffset)
+        path.lineTo(startX + streamerWidth, height.toFloat())
+        path.lineTo(startX, height - streamerHeightOffset)
         path.close()
-
-
     }
 }
